1b2dbbfae985254960562b17493b3f2bb8d61d00
Rodrigo Martins <63435728+Rodriggodev@users.noreply.github.com>
2020-04-30 22:18:34 -0300

    namespace
    namespace relacionado a conflitos de arquivos com o mesmo nome, mas em pastas diferentes

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require 'classes/produto.php';
+require 'models/produto.php';
+
+$produto = new \classes\Produto();
+$produto->mostrarDetalhes();
+
+ ?>
---
+++ produto.php
@@ -0,0 +1,13 @@
+<?php
+namespace classes;// não precisa ser o nome da pasta
+class Produto{
+  public function mostrarDetalhes(){
+    echo "Detalhe do produto da pasta classes";
+
+  }
+}
+
+
+
+
+ ?>
